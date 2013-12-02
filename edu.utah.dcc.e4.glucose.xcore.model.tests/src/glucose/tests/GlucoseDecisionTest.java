305518cfcdab067a06436050b48c989864f4d904
J. Michael Dean <mike.dean@hsc.utah.edu>
2013-12-02 07:08:19 -0700

    Glucose model tests.  Generated from main glucose model xcore plugin.

---
+++ edu.utah.dcc.e4.glucose.xcore.model.tests/src/glucose/tests/GlucoseDecisionTest.java
@@ -0,0 +1,96 @@
+/**
+ */
+package glucose.tests;
+
+import glucose.GlucoseDecision;
+import glucose.GlucoseFactory;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Decision</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class GlucoseDecisionTest extends TestCase
+{
+
+  /**
+   * The fixture for this Decision test case.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected GlucoseDecision fixture = null;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public static void main(String[] args)
+  {
+    TestRunner.run(GlucoseDecisionTest.class);
+  }
+
+  /**
+   * Constructs a new Decision test case with the given name.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public GlucoseDecisionTest(String name)
+  {
+    super(name);
+  }
+
+  /**
+   * Sets the fixture for this Decision test case.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected void setFixture(GlucoseDecision fixture)
+  {
+    this.fixture = fixture;
+  }
+
+  /**
+   * Returns the fixture for this Decision test case.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected GlucoseDecision getFixture()
+  {
+    return fixture;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see junit.framework.TestCase#setUp()
+   * @generated
+   */
+  @Override
+  protected void setUp() throws Exception
+  {
+    setFixture(GlucoseFactory.eINSTANCE.createGlucoseDecision());
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see junit.framework.TestCase#tearDown()
+   * @generated
+   */
+  @Override
+  protected void tearDown() throws Exception
+  {
+    setFixture(null);
+  }
+
+} //GlucoseDecisionTest
